70cc0a32710317e69507deb4b1125d8c7aac63a8
Your Name <you@example.com>
2017-11-22 17:50:51 +0800

    remove 365 type fuyin vid

--- modules/custom/wxapi/src/Controller/WxapiController.php
+++ modules/custom/wxapi/src/Controller/WxapiController.php
@@ -47,88 +47,94 @@ class WxapiController extends ControllerBase {
 		return new JsonResponse([$uid]);
 	}
 	//创建一个grace node from fuyin.tv 0点执行
-	public function graceInit(){
-		$url = 'https://m.fuyin.tv/movie/detail/movid/2784.html';
-		$html = str_get_html(file_get_contents($url));
-    if (!$html) {
-        \Drupal::logger(__FUNCTION__)->notice('error get m.fuyin.tv/');
-        return new JsonResponse(['error get m.fuyin.tv']);
-    }
-    $res=[];
-    $count=0;
-    foreach($html->find('.am-padding-top-xs') as $element) { //init!!!
-    	$count ++; if($count<(date('z')-20)) continue;
-    // $element = $html->find('.am-padding-top-xs',-1);
-    // {
-				$title = $element->find('.am-text-truncate',0)->plaintext;//第02天 20170102单独来面对神
-				$href = $element->find('a',0)->href;// /movie/player/movid/2784/urlid/45054.html
-				preg_match_all('/\d+/', $title,$matches);
-				$date = $matches[0][1]; //20170102
-				preg_match_all('/\d+/', $href,$matches);
-				$video_id = $matches[0][1];// 45054
-				//save node!!!
-				preg_match_all('/第\d+天 \d+(\S+)/',$title,$matches);
-				$title = $matches[1][0];// 45054
-				$time_str = implode('-',[substr($date, 0,4),substr($date, 4,2),substr($date, 6,2) ]) . ' 00:00:00';
-				$created = strtotime($time_str);
+	// public function graceInit(){
+	// 	return  new JsonResponse(['emptyrun']);
+	// 	//TODO delete!!!
+	// 	$url = 'https://m.fuyin.tv/movie/detail/movid/2784.html';
+	// 	$html = str_get_html(file_get_contents($url));
+ //    if (!$html) {
+ //        \Drupal::logger(__FUNCTION__)->notice('error get m.fuyin.tv/');
+ //        return new JsonResponse(['error get m.fuyin.tv']);
+ //    }
+ //    $res=[];
+ //    $count=0;
+ //    foreach($html->find('.am-padding-top-xs') as $element) { //init!!!
+ //    	$count ++; if($count<(date('z')-20)) continue;
+ //    // $element = $html->find('.am-padding-top-xs',-1);
+ //    // {
+	// 			$title = $element->find('.am-text-truncate',0)->plaintext;//第02天 20170102单独来面对神
+	// 			$href = $element->find('a',0)->href;// /movie/player/movid/2784/urlid/45054.html
+	// 			preg_match_all('/\d+/', $title,$matches);
+	// 			$date = $matches[0][1]; //20170102
+	// 			preg_match_all('/\d+/', $href,$matches);
+	// 			$video_id = $matches[0][1];// 45054
+	// 			//save node!!!
+	// 			preg_match_all('/第\d+天 \d+(\S+)/',$title,$matches);
+	// 			$title = $matches[1][0];// 45054
+	// 			$time_str = implode('-',[substr($date, 0,4),substr($date, 4,2),substr($date, 6,2) ]) . ' 00:00:00';
+	// 			$created = strtotime($time_str);
 
-				$fields = ['status'=>1,'created'=>$created];
-				$nid = tools_wxapi_get_nid($fields,'grace');
-				if($nid) {
-					$node = Node::load($nid);
-					if($node->getTitle() !== $title){
-						$node->set('title',$title);
-						$node->set('field_fytv_video_id',$video_id);
-						$node->save();
-						\Drupal::logger('wxapi_cron_init_fy')->notice('Grace %nid title udpated @title.',['%nid'=>$node->id(),'@title'=>$title]);
-					}
-			 		// \Drupal::logger($nid.$time_str)->notice($title);
-					if($time_str === date("Y-m-d 00:00:00")){//today!!!
-						$res[] = $node->id();
-					}
-				}else {
-	       	$newNode = [
-	          'type'             => 'grace',
-	          'created'          => $created,
-	          'changed'          => $created,
-	          'uid'              => 46, //恩典365基督之家 https://api.yongbuzhixi.com/user/46
-	          'title'            => $title,
-	          // An array with taxonomy terms.
-	          'field_fytv_video_id' => [$video_id],
-	          'body'             => [
-	              'summary' => '这里是经文',
-	              'value'   => '内容待填充',
-	              'format'  => 'full_html',
-	          ],
-	        ];
-	        $node = Node::create($newNode);
-	        $node->save();
-	        \Drupal::service('path.alias_storage')->save("/node/" . $node->id(), "/grace/$date", "und");
-				  \Drupal::logger('wxapi_cron_init_fy')->notice('Grace %nid created @title.',['%nid'=>$node->id(),'@title'=>$title]);
-					$res[] = $node->id();
-			}
-    }
-		return new JsonResponse($res);
-	}
+	// 			$fields = ['status'=>1,'created'=>$created];
+	// 			$nid = tools_wxapi_get_nid($fields,'grace');
+	// 			if($nid) {
+	// 				$node = Node::load($nid);
+	// 				if($node->getTitle() !== $title){
+	// 					$node->set('title',$title);
+	// 					$node->set('field_fytv_video_id',$video_id);
+	// 					$node->save();
+	// 					\Drupal::logger('wxapi_cron_init_fy')->notice('Grace %nid title udpated @title.',['%nid'=>$node->id(),'@title'=>$title]);
+	// 				}
+	// 		 		// \Drupal::logger($nid.$time_str)->notice($title);
+	// 				if($time_str === date("Y-m-d 00:00:00")){//today!!!
+	// 					$res[] = $node->id();
+	// 				}
+	// 			}else {
+	//        	$newNode = [
+	//           'type'             => 'grace',
+	//           'created'          => $created,
+	//           'changed'          => $created,
+	//           'uid'              => 46, //恩典365基督之家 https://api.yongbuzhixi.com/user/46
+	//           'title'            => $title,
+	//           // An array with taxonomy terms.
+	//           'field_fytv_video_id' => [$video_id],
+	//           'body'             => [
+	//               'summary' => '这里是经文',
+	//               'value'   => '内容待填充',
+	//               'format'  => 'full_html',
+	//           ],
+	//         ];
+	//         $node = Node::create($newNode);
+	//         $node->save();
+	//         \Drupal::service('path.alias_storage')->save("/node/" . $node->id(), "/grace/$date", "und");
+	// 			  \Drupal::logger('wxapi_cron_init_fy')->notice('Grace %nid created @title.',['%nid'=>$node->id(),'@title'=>$title]);
+	// 				$res[] = $node->id();
+	// 		}
+ //    }
+	// 	return new JsonResponse($res);
+	// }
 	//cron every day!!!
 	public function graceUpdate(){
-		$url = 'http://www.hvsha-tpehoc.com/api/PortalIPS/vwInfoCategory/GetvwInfoByCategoryIDPage?CategoryID=0f4b33a2-be09-4ed5-80d3-c0fc58713680&page=1&pageSize='.'5';
+		$url = 'http://www.hvsha-tpehoc.com/api/PortalIPS/vwInfoCategory/GetvwInfoByCategoryIDPage?CategoryID=0f4b33a2-be09-4ed5-80d3-c0fc58713680&page=1&pageSize=2';
 		$str = file_get_contents($url);
 		$str=json_decode($str);
     $res=[];
-		foreach ($str->data as $key => $item) {
+    $data = array_reverse($str->data);
+		foreach ($data as $key => $item) {
 			$time_str = $item->ShowTime; //2017-09-23 00:00:00
 			$created = strtotime($time_str);
+			$begin_time =  strtotime('2017-01-01 00:00:00');
+			if($created<$begin_time) continue;//not 2016
 			$date = date('Ymd',$created);
-			$fields = ['status'=>1,'created'=>$created];
-			$nid = tools_wxapi_get_nid($fields,'grace');
+			$fields = ['status'=>1,'created'=>$created,'field_tags'=>369];//'uid'=>46,
+			$nid = tools_wxapi_get_nid($fields,'article');
 			if($nid){// load set save
-				$node = Node::load($nid);
-				if($node->body->summary !==$item->Description){
-					$node->body->value = $item->ContentNoHTML;
-					$node->body->summary = $item->Description;
-					$node->save();
-				}
+				$res[] = $nid;
+				// $node = Node::load($nid);
+				// if($node->body->summary !==$item->Description){
+				// 	$node->body->value = $item->ContentNoHTML;
+				// 	$node->body->summary = $item->Description;
+				// 	$node->save();
+				// }
 			}else{//create the node!!!
 				$new_title = explode('》', $item->Title);
 				if(isset($new_title[1])) {
@@ -136,27 +142,38 @@ class WxapiController extends ControllerBase {
 				}else{
 					$new_title=$item->Title;
 				}
+        $body = $item->Content;
+        $body = preg_replace('/style="(.*?)"/', '', strip_tags($body,'<span><p><ul><li><ol><br><section>')) . '<br/>' . $item->Description;
 				$newNode = [
-          'type'             => 'grace',
+          'type'             => 'article',
           'created'          => $created,
           'changed'          => $created,
           'uid'              => 46, //恩典365基督之家 https://api.yongbuzhixi.com/user/46
           'title'            => $new_title,
           // An array with taxonomy terms.
-          // 'field_fytv_video_id' => [$video_id],
+          'field_tags' =>[369],//恩典365
+          'field_article_wechat_term' => [240],//恩典365基督之家 
           'body'             => [
               'summary' => $item->Description,
-              'value'   => $item->ContentNoHTML,
+              'value'   => $body,
               'format'  => 'full_html',
           ],
         ];
+        $fuyintv_has_video_date = strtotime('2017-10-13 00:00:00');
+        // http://xyjs.mov.cnfuyin.com/cdn/01%E7%A6%8F%E9%9F%B3%E8%A7%81%E8%AF%81/%E6%AF%8F%E6%97%A5%E7%81%B5%E4%BF%AE/%E6%81%A9%E5%85%B8365/2017/20171013ED.mp4
+        if($fuyintv_has_video_date >=$created){
+        	$field_video_url = 'http://xyjs.mov.cnfuyin.com/cdn/01%E7%A6%8F%E9%9F%B3%E8%A7%81%E8%AF%81/%E6%AF%8F%E6%97%A5%E7%81%B5%E4%BF%AE/%E6%81%A9%E5%85%B8365/2017/'.date('Ymd',$created).'ED.mp4';
+       		$tmp_res = get_headers($field_video_url,TRUE); 
+        	if(isset($tmp_res[0]) &&  strpos($tmp_res[0], '200' ) !== false){
+        		$newNode['field_video_url'] = $field_video_url;
+        	}
+        }
         $node = Node::create($newNode);
 				$node->save();
-	      \Drupal::service('path.alias_storage')->save("/node/" . $node->id(), "/grace/$date", "und");
-				\Drupal::logger('wxapi_cron_update_tp')->notice('Grace %nid created @title.',['%nid'=>$node->id(),'@title'=>$title]);
+	      \Drupal::service('path.alias_storage')->save("/node/" . $node->id(), "/grace365/$date", "und");
+				\Drupal::logger('wxapi_cron_update_tp')->notice('Grace %nid created @title.',['%nid'=>$node->id(),'@title'=>$new_title]);
+				$res[] = $node->id();
 			}
-
-			$res[] = $node->id();
 		}
 		//每日亲近神
 		$url = 'http://www.hvsha-tpehoc.com/api/PortalIPS/vwInfoCategory/GetvwInfoByCategoryIDPage?CategoryID=2ed87021-5ac1-4fe8-860c-efb88331e859&page=1&pageSize=1';
@@ -166,17 +183,6 @@ class WxapiController extends ControllerBase {
     foreach ($str->data as $key => $item) {
       $time_str = $item->ShowTime; //2017-09-23 00:00:00
       $created = strtotime($time_str);
-      // $date = date('Ymd',$created);
-      // $fields = ['status'=>1,'created'=>$created];//tid=514
-      // $nid = tools_wxapi_get_nid($fields,'article');
-      // if($nid){// load set save
-      //   // $node = Node::load($nid);
-      //   // if($node->body->summary !==$item->Description){
-      //   //   $node->body->value = $item->ContentNoHTML;
-      //   //   $node->body->summary = $item->Description;
-      //   //   $node->save();
-      //   // }
-      // }else
       {//create the node!!!
         $new_title = explode('》', $item->Title);
         if(isset($new_title[1])) {
@@ -206,7 +212,7 @@ class WxapiController extends ControllerBase {
         $node = Node::create($newNode);
         $node->save();
         // \Drupal::service('path.alias_storage')->save("/node/" . $node->id(), "/grace/$date", "und");
-        \Drupal::logger('wxapi_cron_update_tp')->notice('Daily %nid created @title.',['%nid'=>$node->id(),'@title'=>$title]);
+        \Drupal::logger('wxapi_cron_update_tp')->notice('Daily %nid created @title.',['%nid'=>$node->id(),'@title'=>$new_title]);
         // dpm($node->id(),$newNode);
       }
       break;
