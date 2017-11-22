8cb912d86b792e78bc66b55316350d441e130fb5
Your Name <you@example.com>
2017-11-22 15:02:13 +0800

    iframe insert by js

--- modules/custom/mpservice/node.fullview.js
+++ modules/custom/mpservice/node.fullview.js
@@ -21,5 +21,13 @@
     })
     $('.field--name-body p br').remove();
 
+    //add video iframe
+    if('video' in drupalSettings.node){
+      $('.content').prepend('<iframe frameborder="0" width="100%" height="250px" src="//flowplayer.cdn.bcebos.com/index.html?url='+drupalSettings.node.video+'" allowfullscreen></iframe>')
+    }
+    if('audio' in drupalSettings.node){
+      $('.content').prepend('<iframe id="video_top_audio" frameborder="0" width="100%" height="72px" src="//waveplayer.cdn.bcebos.com/nocors.html?url='+drupalSettings.node.audio+'&tiny=0&auto=0" allowfullscreen></iframe>');
+    }
+
   });
 })(jQuery);
