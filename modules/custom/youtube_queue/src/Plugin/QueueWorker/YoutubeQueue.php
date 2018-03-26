eb75601903d7fd69215c0c1063cd3103fbbbf79b
Dale <Dale@BJGLPR-NMX0016.lan>
2018-03-26 16:37:12 +0800

    hook_node_save of youtubequeue

--- modules/custom/youtube_queue/src/Plugin/QueueWorker/YoutubeQueue.php
+++ modules/custom/youtube_queue/src/Plugin/QueueWorker/YoutubeQueue.php
@@ -5,6 +5,7 @@
  */
 namespace Drupal\youtube_queue\Plugin\QueueWorker;
 use Drupal\Core\Queue\QueueWorkerBase;
+use \GuzzleHttp\Exception\RequestException;
 /**
  * A Node Publisher that publishes nodes on CRON run.
  *
@@ -21,13 +22,20 @@ class YoutubeQueue extends QueueWorkerBase {
    */
   public function processItem($data) {
     //Lambda process video!
+    // return lambda_get_youtube_video($data->vid,$data->quality);//720p
     //https://gist.github.com/Jaesin/898a0b74072c7573748b
-    $request = \Drupal::httpClient()
-      ->post('https://l.yongbuzhixi.com/youtube/download/mp4', [
-        'timeout' => 300,
-        'body' => '{"vid":"'.$data->vid.'"}'
-      ]);
-    \Drupal::logger('YoutubeQueue')->notice(json_decode($request->getBody())->message);
-    return $request->getStatusCode()==200?TRUE:FALSE;
+    try{
+      $request = \Drupal::httpClient()
+        ->post('https://l.yongbuzhixi.com/youtube/download/mp4', [
+          'timeout' => 300,
+          'body' => '{"vid":"'.$data->vid.'","quality":"'.$data->quality.'"}'
+        ]);
+      \Drupal::logger('lambda_get_youtube_video')->notice(json_decode($request->getBody())->message);
+      return $request->getStatusCode()==200?TRUE:FALSE;
+    }
+    catch (RequestException $e) {
+      \Drupal::logger(__FUNCTION__.':'.__LINE__)->notice('<pre>'.print_r((array)$e,1));
+      return FALSE;
+    }
   }
 }
